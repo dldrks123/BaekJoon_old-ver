27fb3486cc50d7b1bbcabfc3b54f7ccc6c8431c8
82108 <82108@DESKTOP-6JE3Q5O>
2021-02-07 16:47:07 +0900

    BaekJoon
    No.10950

---
+++ BaekJoon/src/Baekjoon/no10950.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.util.*;
+
+public class no10950 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		int n = input.nextInt();
+		int a = 0;
+		int b = 0;
+
+		for (int i = 0; i < n; i++) {
+			a = input.nextInt();
+			b = input.nextInt();
+			System.out.println(a + b);
+		}
+
+	}
+
+}
